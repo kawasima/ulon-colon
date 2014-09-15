7be093b7e81119b8e3037de47a1ed42ef0b42e40
kawasima <kawasima1016@gmail.com>
2014-09-15 14:16:39 +0900

    release 0.1.0

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject net.unit8/ulon-colon "0.1.0-SNAPSHOT"
+(defproject net.unit8/ulon-colon "0.1.0"
   :description "Message transfer system without a queue."
   :url "https://github.com/kawasima/ulon-colon"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [aleph "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [aleph "0.3.3"]
                  [org.clojure/data.fressian "0.2.0"]
-                 [org.clojure/clojurescript "0.0-2156" :scope "provided"]
+                 [org.clojure/clojurescript "0.0-2280" :scope "provided"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [net.unit8/fressian-cljs "0.1.0"]]
   :plugins [[lein-cljsbuild "1.0.2"]]
